5581bb1daedbdd3b7b136c2592127d1e12aa3254
Shameel Salih <=>
2023-03-23 13:57:57 +0530

    Updates

--- head.php
+++ head.php
@@ -4,6 +4,13 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>LBC - Life Blood Council</title>
+  <meta name="og:title" content="Life Blood Council" />
+  <meta name="og:type" content="Official Website" />
+  <meta name="og:url" content="http://lbc.omsaifoodservice.com" />
+  <meta property="og:image" content="http://lbc.omsaifoodservice.com/images/logo.png" />
+  <meta name="og:site_name" content="Life Blood Council" />
+  <meta name="og:description" content="Blood Donation" />
+  <link rel="icon" href="images/favicon.png">
   <meta name="description" content="Blood Donation">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <link rel="stylesheet" href="css/main-style.css?v=1.0000000000003">
